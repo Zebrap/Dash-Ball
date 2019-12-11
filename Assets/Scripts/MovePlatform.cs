947cb390a9582d9a2c1e0efb32f7620ca2d14c7b
Wojciech Dywan <wojciech.dywan@interia.pl>
2019-12-11 17:20:29 +0100

    MobilePlatorm
    MobilePlatorm

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    private Vector3 orginalPosition;
+    [SerializeField]
+    private Vector3 Velocity;
+    private float endPosR;
+    private float endPosL;
+    private bool right = true;
+
+    void Start()
+    {
+        orginalPosition = transform.position;
+        endPosR = orginalPosition.x + 1;
+        endPosL = orginalPosition.x - 1;
+    }
+    
+
+    private void FixedUpdate()
+    {
+        if (right)
+        {
+            transform.position += (Velocity * Time.deltaTime);
+            if (transform.position.x >= endPosR)
+                right = !right;
+        }
+        else
+        {
+            transform.position -= (Velocity * Time.deltaTime);
+            if (transform.position.x <= endPosL)
+                right = !right;
+        }
+    }
+}
