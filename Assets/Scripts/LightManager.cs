ab51b5ea4a6839e35b6753ac29f65c23f5aba3b3
Wojciech Dywan <wojciech.dywan@interia.pl>
2020-01-03 01:24:27 +0100

    fireflies
    Partile fireflies, Light

--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -6,11 +6,13 @@ public class LightManager : MonoBehaviour
 {
     private GameObject target;
     public GameObject pointLight;
+  //  public GameObject newBallSprite;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("Player");
         pointLight.transform.SetParent(target.transform);
+   //     newBallSprite.transform.SetParent(target.transform);
         
     }
 }
