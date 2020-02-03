f0ecf92f42e51f3509e13f9eee6802583bd6e844
Wojciech Dywan <wojciech.dywan@interia.pl>
2020-02-03 21:19:54 +0100

    CenterAtItem
    Level Selection center at levelReached

---
+++ Assets/Scripts/CenterAtItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterAtItem : MonoBehaviour
+{
+    public RectTransform sr;
+    private void OnEnable()
+    {
+        sr.offsetMax = new Vector2(sr.offsetMax.x, -transform.GetChild(GameManager.Instance.state.levelReached).GetComponent<RectTransform>().anchoredPosition.y);
+    }
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -218,4 +218,5 @@ public class Menu : MonoBehaviour
     {
         GameManager.Instance.ResetSave();
     }
+    
 }
