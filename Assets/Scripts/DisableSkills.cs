938b83c2916e9eb8cc3c270f1983f74473caa00e
Wojciech Dywan <wojciech.dywan@interia.pl>
2020-02-11 13:19:43 +0100

    tutorials

---
+++ Assets/Scripts/DisableSkills.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableSkills : MonoBehaviour
+{
+    private GameObject skillButton;
+    private GameObject skillsPanel;
+
+    void Start()
+    {
+        skillButton = GameObject.Find("SkillButton");
+        skillsPanel = GameObject.Find("SkillsPanel");
+        skillButton.SetActive(false);
+        skillsPanel.SetActive(false);
+    }
+    
+}
---
+++ Assets/Scripts/TutorialSkills.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialSkills : MonoBehaviour
+{
+    private GameObject skillPanel;
+    private Sprite startSprite;
+    private bool changeText = true;
+    public GameObject tutorialPanel;
+    public Sprite useButtonSprite;
+
+    void Start()
+    {
+        skillPanel = GameObject.Find("SkillsPanel");
+        startSprite = skillPanel.transform.GetChild(0).GetComponent<Image>().sprite;
+
+    }
+
+    private void Update()
+    {
+        if(startSprite != skillPanel.transform.GetChild(0).GetComponent<Image>().sprite && changeText)
+        {
+            Debug.Log("Change");
+            tutorialPanel.transform.GetChild(0).GetComponent<Text>().text = "You can use skills during the flight by click the button:";
+            tutorialPanel.transform.GetChild(1).GetComponent<Image>().sprite = useButtonSprite;
+            changeText = false;
+        }
+    }
+
+}
