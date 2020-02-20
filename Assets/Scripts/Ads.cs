2d6489fd251750f6aff8ada75363bbc010f250f6
Wojciech Dywan <wojciech.dywan@interia.pl>
2020-02-20 15:10:19 +0100

    Ads Test

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void ShowAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+
+    private void HandleAdResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Player Gains Gold");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Player Skip ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Player failed to lunch the ad");
+                break;
+        }
+    }
+}
